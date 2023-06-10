154823bb4a2a61a850334f4b3af356f7a3f0c9cb
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-06-10 19:39:13 +0300

    task-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href=${galleryItems.original}>
+      <img class="gallery__image"
+         src=${galleryItems.preview}
+         data-source = ${galleryItems.original}
+         alt ='${galleryItems.description}'>
+       </a>
+        </li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", listItems);
+galleryEl.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  const modalImg = basicLightbox.create(
+    `<img
+        width=100%"
+        height="100%"
+        src = ${e.target.dataset.source}
+      >`,
+    { onShow: () => galleryEl.addEventListener("keydown", onEscKeyPress) }
+  );
+
+  const onEscKeyPress = (e) => {
+    if (e.key === "Escape") modalImg.close();
+  };
+  modalImg.show();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const listItems = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li class="gallery__item"><a class="gallery__link" href=${galleryItems.original}><img class="gallery__image" src=${galleryItems.preview} alt = '${galleryItems.description}'/></a></li>`
+  )
+
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", listItems);
+
+galleryEl.addEventListener("click", onClick);
+
+new SimpleLightbox(".gallery a", { captionDelay: 250, captionsData: "alt" });
+
+function onClick(evt) {
+  evt.preventDefault();
+}
 
 console.log(galleryItems);
